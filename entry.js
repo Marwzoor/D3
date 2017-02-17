399cc482499ad71823334713016317c99335fb7c
Martin Werner <martin@thegeneration.se>
2017-02-17 19:36:18 +0100

    Linear gradients

--- entry.js
+++ entry.js
@@ -10,6 +10,16 @@ var x = d3.scaleTime().range([0, width]);
 var y1 = d3.scaleLinear().range([height, 0]);
 var y2 = d3.scaleLinear().range([height, 0]);
 
+var area1 = d3.area()
+	.x(function (d) { return x(d.date); })
+	.y0(height)
+	.y1(function (d) { return y1(d.humidity); });
+
+var area2 = d3.area()
+	.x(function (d) { return x(d.date); })
+	.y0(height)
+	.y1(function (d) { return y2(d.co2); });
+
 var valueLine1 = d3.line()
 	.x(function (d) { return x(d.date); })
 	.y(function (d) { return y1(d.humidity); });
@@ -37,30 +47,74 @@ d3.csv('data.csv', function (error, data) {
 	y1.domain([0, d3.max(data, function (d) { return d.humidity; })]);
 	y2.domain([0, d3.max(data, function (d) { return d.co2; })]);
 
+	var svgDefs = svg.append("defs");
+
+	var linearGradient1 = svgDefs
+		.append("linearGradient")
+		.attr("id", "data-1-gradient")
+		.attr("x1", "0%")
+		.attr("y1", "0%")
+		.attr("x2", "0%")
+		.attr("y2", "100%");
+
+	linearGradient1.append("stop")
+		.attr("offset", "0%")
+		.attr("stop-color", "rgba(58, 153, 251, 0.5)");
+
+	linearGradient1.append("stop")
+		.attr("offset", "100%")
+		.attr("stop-color", "transparent");
+
+	var linearGradient2 = svgDefs
+		.append("linearGradient")
+		.attr("id", "data-2-gradient")
+		.attr("x1", "0%")
+		.attr("y1", "0%")
+		.attr("x2", "0%")
+		.attr("y2", "100%");
+
+	linearGradient2.append("stop")
+		.attr("offset", "0%")
+		.attr("stop-color", "rgba(0, 248, 184, 0.5)");
+
+	linearGradient2.append("stop")
+		.attr("offset", "100%")
+		.attr("stop-color", "transparent");
+
 	svg.append("path")
 		.data([data])
 		.attr("stroke", "#3a99fb")
-		.attr("class", "line humidity")
+		.attr("class", "line line-1")
 		.attr("d", valueLine1);
 
 	svg.append("path")
 		.data([data])
 		.attr("stroke", "#00f8b8")
-		.attr("class", "line co2")
+		.attr("class", "line line-2")
 		.attr("d", valueLine2);
+
+	svg.append("path")
+		.data([data])
+		.attr("class", "area area-1")
+		.attr("d", area1);
+
+	svg.append("path")
+		.data([data])
+		.attr("class", "area area-2")
+		.attr("d", area2);
 
 	svg.append("g")
 		.attr("transform", "translate(0," + height + ")")
 		.call(d3.axisBottom(x));
 
 	svg.append("g")
-		.attr("class", "humidity")
+		.attr("class", "axis axis-1")
 		.call(d3.axisLeft(y1)
 			.ticks(5)
 			.tickFormat(function (d) { return d + "%"; }));
 
 	svg.append("g")
-		.attr("class", "co2")
+		.attr("class", "axis axis-2")
 		.attr("transform", "translate(" + width + ",0)")
 		.call(d3.axisRight(y2)
 			.ticks(5)
